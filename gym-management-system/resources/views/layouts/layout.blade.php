88ad7bd6bcf5bb84af67c423041bc34e3ae9e675
Mr-Khant <103758712+Myo-Min-Khant292@users.noreply.github.com>
2023-05-08 23:49:01 +0630

    index design changes

--- gym-management-system/resources/views/layouts/layout.blade.php
+++ gym-management-system/resources/views/layouts/layout.blade.php
@@ -29,182 +29,74 @@
   <link href="css/responsive.css" rel="stylesheet" />
 </head>
 <body>   
-    <div class="hero_area">
-        <!-- header section strats -->
-        <header class="header_section">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg custom_nav-container">
-            <a class="navbar-brand" href="index.html">
-                <img src="images/logo.png" alt="" />
-                <span>
-                GLP
-                </span>
-            </a>
-            <div class="contact_nav" id="">
-                <ul class="navbar-nav ">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <img src="images/location.png" alt="" />
-                    <span>Location</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <img src="images/call.png" alt="" />
-                    <span>Call : + 01 1234567890</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                    <img src="images/envelope.png" alt="" />
-                    <span>demo@gmail.com</span>
-                    </a>
-                </li>
-                </ul>
-            </div>
-            </nav>
-        </div>
-
-        </header>
-        <!-- end header section -->
-        <!-- slider section -->
-        <section class=" slider_section position-relative">
-        <div class="container">
-            <div class="custom_nav2">
-            <nav class="navbar navbar-expand-lg custom_nav-container ">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="d-flex  flex-column flex-lg-row align-items-center">
-                    <ul class="navbar-nav  ">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+    <!-- header section strats -->
+    <header class="header_section">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg custom_nav-container">
+                <a class="navbar-brand" href="index.html">
+                    <img src="images/logo.png" alt="" />
+                    <span>
+                    GLP
+                    </span>
+                </a>
+                <div class="contact_nav" id="">
+                    <ul class="navbar-nav ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <img src="images/location.png" alt="" />
+                        <span>Location</span>
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="about.html">About </a>
+                        <a class="nav-link" href="#">
+                        <img src="images/call.png" alt="" />
+                        <span>Call : + 01 1234567890</span>
+                        </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="service.html">Services </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sign-In</a>
+                        <a class="nav-link" href="#">
+                        <img src="images/envelope.png" alt="" />
+                        <span>demo@gmail.com</span>
+                        </a>
                     </li>
                     </ul>
-                    <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                    <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
-                    </form>
                 </div>
-                </div>
-            </nav>
+                </nav>
             </div>
-        </div>
-        <div class="slider_container">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
-                        <div class="detail-box">
-                        <h2>
-                            Get Your Body
-                        </h2>
-                        <h1>
-                            Fitness Here
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam
-                        </p>
-                        <div class="btn-box">
-                            <a href="" class="btn-1">
-                            Read More
-                            </a>
-                            <a href="" class="btn-2">
-                            Get A Quote
-                            </a>
-                        </div>
-                        </div>
+            <div class="container-fluid purple-bg">
+                <div class="custom_nav2">
+                <nav class="navbar navbar-expand-lg custom_nav-container ">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="d-flex  flex-column flex-lg-row align-items-center">
+                        <ul class="navbar-nav  ">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Workout List</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Purchase</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Feedback</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Login</a>
+                            </li>
+                        </ul>                  
                     </div>
                     </div>
-                </div>
-                </div>
-                <div class="carousel-item ">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
-                        <div class="detail-box">
-                        <h2>
-                            Get Your Body
-                        </h2>
-                        <h1>
-                            Fitness Here
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam
-                        </p>
-                        <div class="btn-box">
-                            <a href="" class="btn-1">
-                            Read More
-                            </a>
-                            <a href="" class="btn-2">
-                            Get A Quote
-                            </a>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <div class="carousel-item ">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
-                        <div class="detail-box">
-                        <h2>
-                            Get Your Body
-                        </h2>
-                        <h1>
-                            Fitness Here
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua. Ut enim ad minim veniam
-                        </p>
-                        <div class="btn-box">
-                            <a href="" class="btn-1">
-                            Read More
-                            </a>
-                            <a href="" class="btn-2">
-                            Get A Quote
-                            </a>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+                </nav>
                 </div>
             </div>
-            </div>
-        </div>
-        </section>
-        <!-- end slider section -->
-    </div>
+    </header>
+        <!-- end header section -->
     @yield('content')
 
     <!-- footer section -->
@@ -218,6 +110,7 @@
 
     <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/anchor.js"></script>
 
     <script>
         function openNav() {
--- gym-management-system/resources/views/user/index.blade.php
+++ gym-management-system/resources/views/user/index.blade.php
@@ -2,38 +2,109 @@
 
 @section('content')
 
-  <!-- about section -->
-  <section class="about_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          About GLP
-        </h2>
-      </div>
-      <div class="box">
-        <div class="img-box">
-          <img src="images/about-img.png" alt="">
+  <!-- slider section -->
+  <section class=" slider_section position-relative">
+    <div class="slider_container adj-size">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
+                    <div class="detail-box">
+                    <h2>
+                        Get Your Body
+                    </h2>
+                    <h1>
+                        Fitness Here
+                    </h1>
+                    <p>
+                        Are you ready to take your fitness to the next level? Join our gym and let us help you reach your goals.
+                        Our experienced trainers will guide you every step of the way, and our state-of-the-art equipment will make
+                        your workouts more effective and enjoyable. Don't wait any longer, start your fitness journey today!
+                    </p>
+                    <div class="btn-box">
+                        <a href="#about-us" class="btn-1">
+                        Read More
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="carousel-item ">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
+                    <div class="detail-box">
+                    <h2>
+                        Unleashed Your Inner Fighter
+                    </h2>
+                    <h1>
+                        IN HERE
+                    </h1>
+                    <p>
+                        Boxing is not just a sport, it's a way of life.
+                        Our gym offers top-notch boxing training that will help you improve your strength, 
+                        coordination, and mental toughness. Our experienced trainers will teach you the fundamentals of boxing 
+                        and guide you towards achieving your goals.
+                        Join our gym and become the best version of yourself through boxing!
+                    </p>
+                    <div class="btn-box">
+                        <a href="#about-us" class="btn-1">
+                        Read More
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="carousel-item ">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-6 col-md-7 offset-md-6 offset-md-5">
+                    <div class="detail-box">
+                    <h2>
+                        Aim Your Target Body Weight
+                    </h2>
+                    <h1>
+                        WITH US
+                    </h1>
+                    <p>
+                        Are you ready to transform your body and your life?
+                        Our gym offers personalized support and cutting-edge equipment to help you reach your weight loss goals.
+                        Our experienced trainers will guide you every step of the way, and our supportive community will keep you motivated.
+                        Don't wait any longer, start your weight loss journey today!
+                    </p>
+                    <div class="btn-box">
+                        <a href="#about-us" class="btn-1">
+                        Read More
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
-        <div class="detail-box">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis
-          </p>
-          <a href="">
-            Read More
-          </a>
         </div>
-      </div>
     </div>
   </section>
-  <!-- end about section -->
+  <!-- end slider section -->
 
   <!-- service section -->
   <section class="service_section layout_padding">
     <div class="container">
       <div class="heading_container">
         <h2>
-          Our Services
+          Our Mainn Services
         </h2>
       </div>
       <div class="service_container">
@@ -97,6 +168,76 @@
     </div>
   </section>
   <!-- end service section -->
+
+  <!-- trainer section -->
+  <section class="client_section layout_padding ">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Our Trainers
+        </h2>
+      </div>
+      <div id="carouselExample2Indicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExample2Indicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExample2Indicators" data-slide-to="1"></li>
+          <li data-target="#carouselExample2Indicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/client.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  Trainer 1
+                </h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/client.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  Trainer 2
+                </h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/client.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  Trainer 3
+                </h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- end trainer section -->
 
   <!-- Us section -->
   <section class="us_section layout_padding">
@@ -254,15 +395,41 @@
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
               ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
             </p>
-            <a href="">
-              Get A Quote
-            </a>
           </div>
         </div>
       </div>
     </div>
   </section>
   <!-- end result section -->
+
+  <!-- about section -->
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2 id="about-us">
+          About GLP
+        </h2>
+      </div>
+      <div class="box">
+        <div class="img-box">
+          <img src="images/about-img.png" alt="">
+        </div>
+        <div class="detail-box">
+          <p>
+            Welcome to our fitness center, where we provide a holistic approach to health and wellness.
+            Our state-of-the-art facility offers a wide range of amenities, including a fully-equipped gym,
+            a boxing area, and a variety of cardio machines. We believe that physical fitness is not just about lifting weights
+            or running on a treadmill, but it's about achieving balance in all areas of your life.
+            Our knowledgeable and friendly staff are dedicated to helping you reach your fitness goals,
+            whether that's building strength, increasing endurance, or improving overall health and well-being.
+            With our focus on community, we aim to create a supportive and inclusive environment where everyone feels welcome
+            and encouraged to be their best selves. Come join us and discover the transformative power of fitness!
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end about section -->
 
   <!-- contact section -->
   <section class="contact_section layout_padding">
@@ -320,32 +487,17 @@
   <!-- info section -->
   <section class="info_section layout_padding2-top">
     <div class="container">
-      <div class="info_form">
-        <h4>
-          Our Newsletter
-        </h4>
-        <form action="">
-          <input type="text" placeholder="Enter your email">
-          <div class="d-flex justify-content-end">
-            <button>
-              subscribe
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div class="container">
       <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-4">
           <h6>
-            About Energym
+            About GLP
           </h6>
           <p>
             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
             minim veniam, quis nostrud exercitation u
           </p>
         </div>
-        <div class="col-md-2 offset-md-1">
+        <div class="col-md-3 offset-md-1">
           <h6>
             Menus
           </h6>
@@ -354,51 +506,20 @@
               <a class="" href="index.html">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="">
-              <a class="" href="about.html">About </a>
+              <a class="" href="#">Workout List </a>
             </li>
             <li class="">
-              <a class="" href="service.html">Services </a>
+              <a class="" href="#">Services </a>
             </li>
             <li class="">
-              <a class="" href="#contactSection">Contact Us</a>
+              <a class="" href="#">Purchase</a>
             </li>
             <li class="">
-              <a class="" href="#">Login</a>
+              <a class="" href="#">Feedback</a>
             </li>
           </ul>
         </div>
-        <div class="col-md-3">
-          <h6>
-            Useful Links
-          </h6>
-          <ul>
-            <li>
-              <a href="">
-                Adipiscing
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Elit, sed
-              </a>
-            </li>
-            <li>
-              <a href="">
-                do Eiusmod
-              </a>
-            </li>
-            <li>
-              <a href="">
-                Tempor
-              </a>
-            </li>
-            <li>
-              <a href="">
-                incididunt
-              </a>
-            </li>
-          </ul>
-        </div>
+       
         <div class="col-md-3">
           <h6>
             Contact Us
