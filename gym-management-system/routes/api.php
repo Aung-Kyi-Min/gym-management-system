cd425a3d8dddbd16e29add367bc5b664fb96d4d8
Aung-Kyi-Min <aungkyimin3690@gmail.com>
2023-05-08 13:57:03 +0630

    route_design

--- gym-management-system/routes/api.php
+++ gym-management-system/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/auth/login', [AuthController::class, 'index'])->name('auth.login');
+Route::get('/auth/signup', [AuthController::class, 'index'])->name('auth.signup');
+Route::get('/auth/forgetpassword', [AuthController::class, 'index'])->name('auth.forgetpassword');
+Route::get('/auth/resetpassword', [AuthController::class, 'index'])->name('auth.resetpassword');
+
