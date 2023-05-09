5a849c347586a991a12ed50588a77d28678dca59
Aung-Kyi-Min <aungkyimin3690@gmail.com>
2023-05-09 22:34:16 +0630

    user-profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    public function user_feedback(){
-        return view('user.feedback');
+    public function Userprofile(){
+        return view('user.profile');
     }
 }
