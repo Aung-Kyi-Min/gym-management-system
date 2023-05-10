02dbbcec35b6894a5ad6cd3c20045a7a2949a65c
Aung-Kyi-Min <aungkyimin3690@gmail.com>
2023-05-10 23:14:39 +0630

    done-scm-frame

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,14 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        // Dao Registration
+        $this->app->bind('App\Contracts\Dao\UserDaoInterface', 'App\Dao\UserDao');
+        $this->app->bind('App\Contracts\Dao\AdminDaoInterface', 'App\Dao\AdminDao');
+
+        // Business logic registration
+        $this->app->bind('App\Contracts\Services\UserServiceInterface', 'App\Services\UserService');
+        $this->app->bind('App\Contracts\Services\AdminServiceInterface', 'App\Services\AdminService');
+
     }
 
     /**
