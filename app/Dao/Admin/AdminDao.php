287bead6801b7f18a9ecb834f1dbdf17a9247e1c
Aung-Kyi-Min <aungkyimin3690@gmail.com>
2023-05-15 10:28:28 +0630

    change folder

--- app/Dao/Admin/AdminDao.php
+++ app/Dao/Admin/AdminDao.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Dao;
+namespace App\Dao\Admin;
 
-use App\Contracts\Dao\AdminDaoInterface;
+use App\Contracts\Dao\Admin\AdminDaoInterface;
 use App\Models\Admin;
 
 class AdminDao implements AdminDaoInterface
 {
-    
+
 }
