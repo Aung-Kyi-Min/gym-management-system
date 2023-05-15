fb24eddd27897f34eeed31d4a0dc7f3ace1b672d
Aung-Kyi-Min <aungkyimin3690@gmail.com>
2023-05-15 13:50:40 +0630

    import_member

--- app/Imports/MembersImport.php
+++ app/Imports/MembersImport.php
@@ -3,54 +3,26 @@
 namespace App\Imports;
 
 use App\Models\Member;
+use Illuminate\Support\Carbon;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Illuminate\Support\Carbon;
-use Maatwebsite\Excel\Concerns\ToCollection;
-//use Ramsey\Collection\Collection;
-use Illuminate\Support\Collection;
 
-class MembersImport implements ToCollection , WithHeadingRow
+class MembersImport implements ToModel, WithHeadingRow
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function collection(Collection $rows)
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
     {
-        dd(Carbon::parse($rows['joining_date'])->format('Y-m-d H:i:s'));
-        foreach ($rows as $row) {
-            Member::create([
-                //'name' => $row['name'],
-                //'email' => $row['email'],
-                //'password' => bcrypt($row['password']),
-                'user_id' => $row['user_id'],
-                'workout_id' => $row['workout_id'],
-                'instructor_id' => $row['instructor_id'],
-                'sub_month' => $row['sub_month'],
-                //$date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['datetime_column']);
-                //'joining_date' => $row['datetime_column'] = $date->format('Y-m-d H:i:s'),
-                'joining_date' => Carbon::parse($row['joining_date'])->format('Y-m-d H:i:s'),
-                'end_date' => Carbon::parse($row['end_date'])->format('Y-m-d H:i:s'),
-            ]);
-        }
+        return new Member([
+            'user_id' => $row['user_id'],
+            'workout_id' => $row['workout_id'],
+            'instructor_id' => $row['instructor_id'],
+            'sub_month' => $row['sub_month'],
+            'joining_date' => Carbon::parse($row['joining_date'])->format('Y-m-d H:i:s'),
+            'end_date' => Carbon::parse($row['end_date'])->format('Y-m-d H:i:s'),
+        ]);
     }
-    //public function model(array $row)
-    //{
-    //    //dd(Carbon::parse('5/22/2022')->format('Y-m-d H:i:s'));
-    //    //dd($row);
-    //    return new Member([
-    //        'user_id' => $row['user_id'],
-    //        'workout_id' => $row['workout_id'],
-    //        'instructor_id' => $row['instructor_id'],
-    //        'sub_month' => $row['sub_month'],
-    //        'joining_date' => Carbon::parse($row['joining_date'])->format('Y-m-d H:i:s'),
-    //        'end_date' => Carbon::parse($row['end_date'])->format('Y-m-d H:i:s'),
-    //        //'joining_date' => Carbon::format('d-m-Y H:i:s', $row['joining_date']),
-    //        //'end_date' => Carbon::format('d-m-Y H:i:s', $row['end_date']),
-    //        //'joining_date' => $row['joining_date']->date_format('d-m-Y'),
-    //        //'end_date' => $row['end_date']->date_format('d-m-Y'),
-    //    ]);
-    //}
 }
