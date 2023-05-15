9c2be6d51515e14e2ec16889d09de80ce6f07bf1
Aung-Kyi-Min <aungkyimin3690@gmail.com>
2023-05-15 14:53:53 +0630

    phone_limit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->longText('address');
             $table->string('gender');
             $table->integer('age');
-            $table->string('phone');
+            $table->string('phone', 20);
             $table->rememberToken();
             $table->timestamps();
         });
